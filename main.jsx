53f964ac8b5a0b9623c2015fb9ac060d6b45ebb4
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-07 22:57:05 +0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OptOrderPage from './OptOrderPage.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OptOrderPage />
+  </React.StrictMode>,
+)
