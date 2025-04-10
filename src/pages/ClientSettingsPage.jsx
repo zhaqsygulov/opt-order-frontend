b0de25d1fbcc059705a53c6528cbf58ad96bb0b9
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-10 23:52:36 +0500

    Create ClientSettingsPage.jsx

---
+++ src/pages/ClientSettingsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ClientSettingsPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Настройки клиента</h1>
+      <p className="mt-2 text-gray-600">Здесь будут настройки профиля клиента.</p>
+    </div>
+  )
+}
