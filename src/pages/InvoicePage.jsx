4f2779b7a012ef2e690317d086dc855373daa630
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-10 23:59:04 +0500

    Create InvoicePage.jsx

---
+++ src/pages/InvoicePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function InvoicePage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Счета</h1>
+      <p className="mt-2 text-gray-600">Здесь будет отображение счетов клиентов.</p>
+    </div>
+  )
+}
