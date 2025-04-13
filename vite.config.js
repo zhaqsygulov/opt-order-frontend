6ee817b8f2ac10459579fb8313bf409ef5eb6a65
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-13 16:05:07 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ms-onec-plugin-master-3.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
