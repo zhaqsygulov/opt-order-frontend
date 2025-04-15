b6dc08cafe53f66c97f1acb7e987008c6c228957
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-15 13:19:24 +0500

    Update ClientSettingsPage.jsx

--- src/pages/ClientSettingsPage.jsx
+++ src/pages/ClientSettingsPage.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { saveClientSettings } from "@/api/backend";
 
 const ClientSettingsStep = ({ onNext, contextData }) => {
