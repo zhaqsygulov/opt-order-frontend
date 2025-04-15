510acb2ffeca72b333ee113e21a202b3fd10f68b
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-15 13:00:42 +0500

    Update ContextCheckStep.jsx

--- src/pages/ContextCheckStep.jsx
+++ src/pages/ContextCheckStep.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; // 👈 добавлен React
 import axios from "axios";
 
 const ContextCheckStep = ({ onNext, setContextData }) => {
